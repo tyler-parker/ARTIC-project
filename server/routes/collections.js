98cb4a74bc51d01913d89dc690e9a64e77cfdcf5
Tyler Parker <tparker42d2@gmail.com>
2021-09-10 14:03:42 -0600

    installed mongoose@5.8.1

--- server/routes/collections.js
+++ server/routes/collections.js
@@ -2,6 +2,6 @@ const express = require('express')
 const collectionsRouter = express.Router()
 const axios = require('axios')
 
-collectionsRouter.get('/:departmentId', (req, res, next) => {
+collectionsRouter.get('/exhibits/:departmentId', (req, res, next) => {
     axios.get('https://collectionapi.metmuseum.org/public/collection/v1/objects')
 })
