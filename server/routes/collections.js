f1388f2f74f3bb23bffefadcf9e448454f87bfd0
Tyler Thomas <tygthomas0@gmail.com>
2021-09-10 14:56:19 -0600

    added server connection

--- server/routes/collections.js
+++ server/routes/collections.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const collectionsRouter = express.Router()
-const axios = require('axios')
+//const modelName = model
 
 collectionsRouter.get('/exhibits/:departmentId', (req, res, next) => {
     axios.get('https://collectionapi.metmuseum.org/public/collection/v1/objects')
