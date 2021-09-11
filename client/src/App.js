355dd6e13761aba9bfa97534dc12fa74922ee9bf
Tyler Thomas <tygthomas0@gmail.com>
2021-09-10 18:48:39 -0600

    restructured the filepaths

---
+++ client/src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Grid,
+  theme
+} from '@chakra-ui/react';
+import Hero from './pages/Hero';
+import NavBar from './components/NavBar';
+import Exhibits from './pages/Exhibits';
+import About from './pages/About';
+import Contact from "./pages/Contact"
+import Collection from './pages/Collection';
+
+import {
+  Switch,
+  Route,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl" bgColor="#191919">
+        <Grid minH="100vh" >
+          <NavBar />
+          <Switch>
+            <Route exact path='/'>
+                <Hero />
+            </Route>
+            <Route path='/exhibits'>
+              <Exhibits />
+            </Route>
+            <Route path='/about'>
+              <About />
+            </Route>
+            <Route path='/contact'>
+              <Contact />
+            </Route>
+            <Route path='/collection'>
+              <Collection />
+            </Route>
+          </Switch>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App
--- server/server.js
+++ server/server.js
@@ -1,15 +1,28 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
 app.use(express.json())
 app.use(morgan('dev'))
 
-// app.use('/collections', require('./routes/collections'))
+mongoose.connect('mongodb://localhost:27017/metdb',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    },
+    () => console.log('Connected to the Met Database!')
+)
 
-app.get('/', (req, res) => {
-    res.send('Server started')
+app.use("/collections", require("./routes/collections.js"))
+
+app.use((err, req, res, next) => {
+    console.log(err)
+    return res.send({errMsg: err.message})
 })
+
 app.listen(9000, () => {
     console.log('The server is running on port 9000');
 })
