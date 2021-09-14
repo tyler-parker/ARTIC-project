b77594b293839fe9a00de1a5c51d4e8cd4223c97
Tyler Parker <tparker42d2@gmail.com>
2021-09-13 20:45:22 -0600

    added some styling with the masonry package on the Collection page

--- client/src/pages/Collection.jsx
+++ client/src/pages/Collection.jsx
@@ -5,13 +5,15 @@ import {
     Image
 } from '@chakra-ui/react'
 import axios from 'axios'
+import Masonry from 'react-masonry-css'
+import './collection.css'
 
 export default function Collection(props) {
     const [queryObjIds, setQueryObj] = useState([])
     const [singleObjIds, setSingleObjIds] = useState([])
 
     useEffect(() => {
-        axios.get(`https://api.artic.edu/api/v1/artworks/search?q=stone&fields=id,title,image_id,artist_display&limit=20`)
+        axios.get(`https://api.artic.edu/api/v1/artworks/search?q=stone&fields=id,title,image_id,artist_display&limit=80`)
             .then(res => setQueryObj(res.data.data))
             .catch(err => console.log(err))
     }, [])
@@ -20,26 +22,36 @@ export default function Collection(props) {
         axios.get(`https://api.artic.edu/api/v1/artworks?ids=4453,109467,11625,87451,23365&fields=id,title,image_id`)
             .then(res => setSingleObjIds(res.data.data))
             .catch(err => console.log(err))
-    })
+    }, [])
 
-    const queryArtImages = queryObjIds.map(obj => <Image key={obj.id} src={`https://www.artic.edu/iiif/2/${obj.image_id}/full/843,/0/default.jpg`} />)
-    const singleArtImages = singleObjIds.map(obj => <Image key={obj.id} src={`https://www.artic.edu/iiif/2/${obj.image_id}/full/843,/0/default.jpg`} />)
+    const queryArtImages = queryObjIds.map(obj => <Image p={2} key={obj.id} src={`https://www.artic.edu/iiif/2/${obj.image_id}/full/843,/0/default.jpg`} />)
+    const singleArtImages = singleObjIds.map(obj => <Image p={2} key={obj.id} src={`https://www.artic.edu/iiif/2/${obj.image_id}/full/843,/0/default.jpg`} />)
     
     return (
         <>
             <Heading >
                 Queried Collection
             </Heading>
-            <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+            <Masonry
+                breakpointCols={3}
+                className="my-masonry-grid"
+                columnClassName="my-masonry-grid_column"
+            >
                 {queryArtImages}
-            </Grid>
+            </Masonry>
 
             <Heading >
                 Single Collection
             </Heading>
-            <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+            <Masonry
+                breakpointCols={3}
+                className="my-masonry-grid"
+                columnClassName="my-masonry-grid_column"
+            >
                 {singleArtImages}
-            </Grid>
+            </Masonry>
+            {/* <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+            </Grid> */}
         </>
     )
 }
