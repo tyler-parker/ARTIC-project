f9bb2e8517e24fbb004e5c06e33e840b15fb6d6c
Tyler Parker <tparker42d2@gmail.com>
2021-09-16 09:26:43 -0600

    fleshed out the ArtPiece page and can successfully map data for the selected art piece. Need to know what data we should display.

--- client/src/pages/ArtPiece.jsx
+++ client/src/pages/ArtPiece.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import {
+    Image,
+    VStack,
+    Text,
+    Grid,
+    GridItem
+} from '@chakra-ui/react'
+import axios from 'axios'
 
 export default function ArtPiece() {
+    const [artPiece, setArtPiece] = useState([])
+    const { id } = useParams()
+
+    useEffect(() => {
+        axios.get(`https://api.artic.edu/api/v1/artworks/${id}`)
+            .then(res => setArtPiece(res.data.data))
+            .catch(err => console.log(err))
+    }, [])
+
     return (
         <>
-            
+
+                <Grid justifyContent='center' templateColumns="1fr 1fr" margin={4}>
+                    <GridItem>
+                        <Image src={`https://www.artic.edu/iiif/2/${artPiece.image_id}/full/600,/0/default.jpg`} />
+                    </GridItem>
+                    <GridItem>
+                        <VStack>
+                            <Text fontSize='3xl' fontFamily='Montserrat'>{artPiece.title}</Text>
+                            <Text fontSize='xl' fontFamily='Montserrat'>{artPiece.artist_display}</Text>
+                        </VStack>
+                    </GridItem>
+                </Grid>
+
         </>
     )
 }
